bcdad3df2e13e2f00960ba528569d12f9adae95d
williampettit <14142910+williampettit@users.noreply.github.com>
2024-12-16 00:25:21 -0500

    day2 p1&2

---
+++ day2p1.py
@@ -0,0 +1,29 @@
+def is_safe(nums: list[int]) -> bool:
+  if nums[0] > nums[1]:
+    nums.reverse()
+  
+  for i in range(len(nums) - 1):
+    curr_num = nums[i]
+    next_num = nums[i + 1]
+    if next_num <= curr_num:
+      return False
+    if next_num - 3 > curr_num:
+      return False
+  return True
+
+def main():
+  with open("./data/2.in") as fh:
+    lines = fh.readlines()
+
+  total_safe = 0
+  for line in lines:
+    nums = line.split()
+    nums = [int(x) for x in nums]
+    
+    if is_safe(nums):
+      total_safe += 1
+  
+  print(total_safe)
+
+if __name__ == "__main__":
+  main()
